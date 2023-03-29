0506248c60c95c147c5982ce8fa31aa4dbb52e69
volodymyr <vova.shunkarenko@gmail.com>
2023-03-29 16:23:53 +0300

    Initial commit

---
+++ src/main/java/mvp/Main.java
@@ -0,0 +1,22 @@
+package mvp;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (InputStream stream = Main.class.getClassLoader().getResourceAsStream("fullName.json")) {
+            Gson gson = new GsonBuilder()
+                    .setPrettyPrinting()
+                    .serializeNulls()
+                    .create();
+
+            NameDto dto = gson.fromJson(new String(stream.readAllBytes()), NameDto.class);
+            String toJson = gson.toJson(dto);
+            System.out.println(toJson);
+        }
+    }
+}
---
+++ src/main/java/mvp/NameDto.java
@@ -0,0 +1,14 @@
+package mvp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class NameDto {
+    private String name;
+    private String lastName;
+
+}
